8fd17bace461f9291cc98056b2561da6521264d7
Ngoakoana <ngoakoana.mphago460@gmail.com>
2025-01-21 14:10:39 +0200

    added all component and daatabase

--- client/App/src/app/app.routes.ts
+++ client/App/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ProductFormComponent } from './product-form/product-form.component';
+import { ProductListComponent } from './productlist/productlist.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent},
+  { path: 'add', component: ProductFormComponent },
+  { path: 'edit/:id', component: ProductFormComponent }
+];
